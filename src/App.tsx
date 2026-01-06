46b94f2ec6aac0b6bfc10165484c5936523f96f9
belunatic <abelluvyou@yahoo.com>
2026-01-06 11:34:06 -0800

    add next button

--- src/App.tsx
+++ src/App.tsx
@@ -22,14 +22,26 @@ function App() {
 		getRandomQuestionNumber();
 	}, []);
 
+	const handleNext = () => {
+		const getNumber = Math.floor(Math.random() * comboQuestion.length);
+		console.log(getNumber);
+		//set the question to currentQuestion state
+		setCurrentQuestion(getNumber);
+	};
+
 	return (
-		<section className="min-h-screen dark:bg-zinc-800 dark:text-white">
-			<main className="max-w-[85rem] h-screen w-auto flex justify-center items-center p-4">
-				<h1 className="text-2xl font-bold">
+		<main className="min-h-screen dark:bg-zinc-800 dark:text-white">
+			<section className="max-w-[85rem] h-screen w-auto flex flex-col gap-y-8 justify-center items-center p-4">
+				<h1 className="text-2xl font-bold text-center">
 					{currentQuestion ? comboQuestion[currentQuestion] : ""}
 				</h1>
-			</main>
-		</section>
+				<button
+					className="px-4 py-2 rounded-sm bg-blue-500 cursor-pointer"
+					onClick={handleNext}>
+					Next
+				</button>
+			</section>
+		</main>
 	);
 }
 
