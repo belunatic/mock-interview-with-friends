0e33110e7923fe448b6dc7d2daf0408902268368
belunatic <abelluvyou@yahoo.com>
2026-01-06 10:37:21 -0800

    setup the tailwind and inital design

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
 	return (
-		<>
-			<h1>Hello World</h1>
-		</>
+		<section className="min-h-screen dark:bg-zinc-800 dark:text-white">
+			<main className="max-w-[85rem] h-screen w-auto flex justify-center items-center">
+				<h1>Hello World</h1>
+			</main>
+		</section>
 	);
 }
 
