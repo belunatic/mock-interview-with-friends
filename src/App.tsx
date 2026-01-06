2cc4b71da212c0964621c89d6bdbdd43d3bd1fb2
belunatic <abelluvyou@yahoo.com>
2026-01-06 11:11:43 -0800

    set up useEffect for initial question

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { technicalQuestions, behaviorQuestions } from "./util/questions";
+import { useState, useEffect } from "react";
+
 function App() {
+	//current question
+	const [currentQuestion, setCurrentQuestion] = useState<number | null>(null);
+	//combineQuestion
+	const [comboQuestion, setComboQuestion] = useState([
+		...technicalQuestions,
+		...behaviorQuestions,
+	]);
+
+	useEffect(() => {
+		//get a random question
+		const getRandomQuestionNumber = () => {
+			const getNumber = Math.floor(Math.random() * comboQuestion.length);
+			console.log(getNumber);
+			//set the question to currentQuestion state
+			setCurrentQuestion(getNumber);
+		};
+
+		getRandomQuestionNumber();
+	}, []);
+
 	return (
 		<section className="min-h-screen dark:bg-zinc-800 dark:text-white">
 			<main className="max-w-[85rem] h-screen w-auto flex justify-center items-center">
-				<h1>Hello World</h1>
+				<h1>{currentQuestion ? comboQuestion[currentQuestion] : ""}</h1>
 			</main>
 		</section>
 	);
