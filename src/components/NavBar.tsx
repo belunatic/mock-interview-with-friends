23f16a82e7948890dbf5fc0b6711d9c7816dacfd
belunatic <abelluvyou@yahoo.com>
2026-03-25 10:08:30 -0700

    changed the navbar color

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,7 @@ import SearchBar from "./SearchBar";
 function NavBar() {
 	return (
 		<nav
-			className="flex justify-between md:justify-end gap-10 h-15 border-1 px-5 bg-gray-300 text-zinc-900 dark:bg-zinc-400 dark:text-white
+			className="flex justify-between md:justify-end gap-10 h-15 border-1 px-5 bg-gray-300 text-zinc-900 dark:bg-zinc-600 dark:text-white
         ">
 			<NavLink to="/" className="mt-4">
 				Home
